bfbd26451bb1fc747943d483f298b1a0a36b42e1
TerryTaoYY <yueyang.tao@gmail.com>
2025-10-15 17:50:32 -0400

    addScheduledTaskFeature

--- src/main/java/com/example/demoproject/task/ScheduledTask.java
+++ src/main/java/com/example/demoproject/task/ScheduledTask.java
@@ -1,4 +1,31 @@
 package com.example.demoproject.task;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class ScheduledTask {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void task1() {
+        String time = LocalDateTime.now().format(formatter);
+        System.out.println("【定时任务1】每5秒执行一次 - 当前时间: " + time);
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void task2() {
+        String time = LocalDateTime.now().format(formatter);
+        System.out.println("【定时任务2】每10秒执行一次 - 当前时间: " + time);
+    }
+
+    @Scheduled(initialDelay = 3000, fixedRate = 60000)
+    public void task3() {
+        String time = LocalDateTime.now().format(formatter);
+        System.out.println("【定时任务2】每10秒执行一次 - 当前时间:  " + time);
+    }
+
 }
